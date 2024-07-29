63fac258cbbfb0936c26310b3519378716f447c6
Chris Gonzalez <christogonz92@gmail.com>
2024-07-29 14:58:36 +0200

    Initial Commit

---
+++ ExpandAnimation/ExpandAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandAnimationApp.swift
+//  ExpandAnimation
+//
+//  Created by Christopher Gonzalez on 2024-07-29.
+//
+
+import SwiftUI
+
+@main
+struct ExpandAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
